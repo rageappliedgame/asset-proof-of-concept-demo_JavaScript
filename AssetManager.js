ad86402be57eef90ba3e35ed4a31062fb4f07845
G.W. van der Vegt <wvd_vegt@180950.no-reply.drupal.org>
2015-03-23 15:12:59 +0100

    Initial check-in

---
+++ AssetManager.js
@@ -0,0 +1,363 @@
+﻿/// <summary>
+/// A Simple Asset Manager class that registers instances of classes and supplies some
+/// bookkeeping methods to allow inter asset communication.
+/// 
+/// Based on a pattern in http://addyosmani.com/resources/essentialjsdesignpatterns/book/.
+/// 
+/// TODO add loadScriptAsync/loadScriptSync to AssetManager's public interface.
+/// TODO typescript implementation.
+/// </summary>
+var AssetManager = (function () {
+
+    /// <summary>
+    /// Instance stores a reference to the Singleton.
+    /// </summary>
+    var instance;
+
+    /// <summary>
+    /// See http://stackoverflow.com/questions/950087/include-a-javascript-file-in-another-javascript-
+    /// file.
+    /// </summary>
+    ///
+    /// <param name="url">      URL of the document. </param>
+    /// <param name="callback"> The callback. </param>
+    ///
+    /// <returns>
+    /// The script.
+    /// </returns>
+    function loadScriptAsync(url, callback) {
+        // Adding the script tag to the head as suggested before
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+
+        // Then bind the event to the callback function.
+        // There are several events for cross browser compatibility.
+        script.onreadystatechange = callback;
+        script.onload = callback;
+
+        // Fire the loading
+        head.appendChild(script);
+    }
+
+    /// <summary>
+    /// See http://stackoverflow.com/questions/2879509/dynamically-loading-javascript-synchronously
+    /// See https://github.com/addyosmani/pubsubz.
+    /// </summary>
+    ///
+    /// <param name="url"> URL of the document. </param>
+    function loadScriptSync(url) {
+        var xhrObj = new XMLHttpRequest();
+        xhrObj.open('GET', 'pubsubz.js', false);
+        xhrObj.send(null);
+        eval(xhrObj.responseText);
+    }
+
+    // Based on http://www.mojavelinux.com/articles/javascript_hashes.html
+    // © Mojavelinux, Inc
+    // Document Version: 1.3
+    // Last Modified: Fri Mar 4, 2012
+    // License: Creative Commons Attribution-ShareAlike 3.0 Unported License
+    // 
+    function HashTable(obj) {
+
+        /// <summary>
+        /// Init.
+        /// </summary>
+        this.length = 0;
+
+        /// <summary>
+        /// An object that holds the hash table items.
+        /// 
+        /// The keys are property names, 
+        /// the property values are the objects stored in the hast table.
+        /// </summary>
+        this.items = {};
+
+        // Process obj parameter if any. 
+        for (var p in obj) {
+            if (obj.hasOwnProperty(p)) {
+                this.items[p] = obj[p];
+                this.length++;
+            }
+        }
+
+        /// <summary>
+        /// Sets an item.
+        /// </summary>
+        ///
+        /// <param name="key">   The key. </param>
+        /// <param name="value"> The value. </param>
+        ///
+        /// <returns>
+        /// The previous value if defined.
+        /// </returns>
+        this.setItem = function (key, value) {
+            var previous = undefined;
+            if (this.hasItem(key)) {
+                previous = this.items[key];
+            }
+            else {
+                this.length++;
+            }
+            this.items[key] = value;
+            return previous;
+        }
+
+        /// <summary>
+        /// Gets an item.
+        /// </summary>
+        ///
+        /// <param name="key"> The key. </param>
+        ///
+        /// <returns>
+        /// The item.
+        /// </returns>
+        this.getItem = function (key) {
+            return this.hasItem(key) ? this.items[key] : undefined;
+        }
+
+        /// <summary>
+        /// Has item.
+        /// </summary>
+        ///
+        /// <param name="key"> The key. </param>
+        ///
+        /// <returns>
+        /// true if the item for the key is found
+        /// </returns>
+        this.hasItem = function (key) {
+            return this.items.hasOwnProperty(key);
+        }
+
+        /// <summary>
+        /// Removes the item described by key.
+        /// </summary>
+        ///
+        /// <param name="key"> The key. </param>
+        ///
+        /// <returns>
+        /// The value of the removed key.
+        /// </returns>
+        this.removeItem = function (key) {
+            if (this.hasItem(key)) {
+                previous = this.items[key];
+                this.length--;
+                delete this.items[key];
+                return previous;
+            }
+            else {
+                return undefined;
+            }
+        }
+
+        /// <summary>
+        /// Keys this object.
+        /// 
+        /// If used with foreach it returns the indexes and not the key values themselves.
+        /// </summary>
+        ///
+        /// <returns>
+        /// The keys of the hash table.
+        /// </returns>
+        this.keys = function () {
+            var keys = [];
+            for (var k in this.items) {
+                if (this.hasItem(k)) {
+                    keys.push(k);
+                }
+            }
+            return keys;
+        }
+
+        /// <summary>
+        /// Values this object.
+        /// </summary>
+        ///
+        /// <returns>
+        /// The values in the hash table.
+        /// </returns>
+        this.values = function () {
+            var values = [];
+            for (var k in this.items) {
+                if (this.hasItem(k)) {
+                    values.push(this.items[k]);
+                }
+            }
+            return values;
+        }
+
+        /// <summary>
+        /// Applies a method to each of the hash table items.
+        /// </summary>
+        ///
+        /// <param name="fn"> The function. </param>
+        this.each = function (fn) {
+            for (var k in this.items) {
+                if (this.hasItem(k)) {
+                    fn(k, this.items[k]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears this object to its blank/initial state.
+        /// </summary>
+        this.clear = function () {
+            this.items = {}
+            this.length = 0;
+        }
+
+    }
+
+    /// <summary>
+    /// The test subscriber.
+    /// </summary>
+    var testSubscriber;
+
+    /// <summary>
+    /// The test subscription.
+    /// </summary>
+    var testSubscription;
+
+    /// <summary>
+    /// The initialise event system.
+    /// 
+    /// See https://github.com/addyosmani/pubsubz
+    /// 
+    /// Note: because of the setTimeout used in pubSubz the assets subscribing later will get to see this event.
+    /// </summary>
+    var initEventSystem = function () {
+        pubsubz.define('EventSystem.Init');
+
+        pubsubz.publish('EventSystem.Init', 'hello event!');
+    };
+
+    /// <summary>
+    /// The assets hash table.
+    /// </summary>
+    var assets = new HashTable();
+
+    /// <summary>
+    /// Initialises this object.
+    /// </summary>
+    ///
+    /// <returns>
+    /// The asset manager.
+    /// </returns>
+    function init() {
+
+        // Singleton
+        //
+        console.log("AssetManager.init()")
+
+        // Async external script loading.
+        // 
+        // loadScript("pubsubz.js", initEventSystem);
+
+        // See http://stackoverflow.com/questions/2879509/dynamically-loading-javascript-synchronously
+        // See https://github.com/addyosmani/pubsubz
+        //
+        loadScriptSync('pubsubz.js');
+
+        initEventSystem();
+
+        console.log("After initEventSystem");
+
+        // Private methods and variables
+        // 
+        //function privateMethod() {
+        //    console.log("I am private");
+        //}
+
+        // User to generate uniqueId's for registered assets.
+        // 
+        var idGenerator = 0;
+
+        // var privateVariable = "Im also private";
+
+        // var privateRandomNumber = Math.random();
+
+        return {
+
+            // Public methods and variables
+            // 
+            //publicMethod: function () {
+            //    console.log("The public can see me!");
+            //},
+
+            //publicProperty: "I am also public",
+
+            //getRandomNumber: function () {
+            //    return privateRandomNumber;
+            //},
+
+            // Register an Asset.
+            // 
+            registerAssetInstance: function (asset, claz) {
+                // console.log(name);
+                // This does not work if we can remove or rename items! 
+                // Unless the length of the array never shrinks.
+                var id = claz + "_" + (idGenerator++).toString(); //assets.length
+
+                asset.id = id;
+
+                console.log("Registering Asset " + typeof (asset) + "/" + claz + " as " + id);
+
+                assets.setItem(id, asset);
+
+                console.log("Registered " + assets.length + " Asset(s)");
+
+                return id;
+            },
+
+            // Returns the exact match.
+            // 
+            findAssetById: function (id) {
+                return assets.getItem(id);
+            },
+
+            // Returns the first match.
+            // 
+            findAssetByClass: function (claz) {
+                var keys = assets.keys();
+
+                for (i = 0; i < assets.length; i++)
+                    if (keys[i].indexOf(claz + "_") == 0) {
+                        return assets.getItem(keys[i]);
+                    }
+
+                return null;
+            },
+
+            findAssetsByClass: function (claz) {
+                var keys = assets.keys();
+                var results = [];
+
+                for (i = 0; i < assets.length; i++)
+                    if (keys[i].indexOf(claz + "_") == 0) {
+                        results.push(assets.getItem(keys[i]));
+                    }
+
+                return results;
+            },
+        };
+    };
+
+    // Return (and create once) the instance.
+    // 
+    return {
+
+        // Get the Singleton instance if one exists
+        // or create one if it doesn't
+        getInstance: function () {
+            if (!instance) {
+                instance = init();
+            }
+
+            return instance;
+        },
+    };
+
+})();
---
+++ pubsubz.js
@@ -0,0 +1,96 @@
+﻿/*!
+* Pub/Sub implementation
+* http://addyosmani.com/
+* Licensed under the GPL
+* http://jsfiddle.net/LxPrq/
+*/
+
+// todo error checks on nil/undefined objects (as func in subscribers[len].func(topic, args) ?).
+// todo add sender in publish (or is args enough)?
+// todo add some more bookkeeping (like list events).
+// todo use the hashtable from AssetManager instead (so rewrite this code)?
+// 
+// done added pubsubz.define(topic) method.
+// 
+; (function (window, doc, undef) {
+
+    var topics = {},
+        subUid = -1,
+        pubsubz = {};
+
+    /// <summary>
+    /// Defines this object.
+    /// </summary>
+    ///
+    /// <param name="topic"> The topic. </param>
+    pubsubz.define = function (topic) {
+
+        if (!topics[topic]) {
+            topics[topic] = [];
+        }
+    }
+
+    /// <summary>
+    /// Publishes.
+    /// </summary>
+    ///
+    /// <param name="topic"> The topic. </param>
+    /// <param name="args">  The arguments. </param>
+    ///
+    /// <returns>
+    /// true if the topic exists
+    /// </returns>
+    pubsubz.publish = function (topic, args) {
+
+        if (!topics[topic]) {
+            return false;
+        }
+
+        setTimeout(function () {
+            var subscribers = topics[topic],
+                len = subscribers ? subscribers.length : 0;
+
+            while (len--) {
+                subscribers[len].func(topic, args);
+            }
+        }, 0);
+
+        return true;
+
+    };
+
+    pubsubz.subscribe = function (topic, func) {
+
+        if (!topics[topic]) {
+            topics[topic] = [];
+        }
+
+        var token = (++subUid).toString();
+        topics[topic].push({
+            token: token,
+            func: func
+        });
+        return token;
+    };
+
+    pubsubz.unsubscribe = function (token) {
+        for (var m in topics) {
+            if (topics[m]) {
+                for (var i = 0, j = topics[m].length; i < j; i++) {
+                    if (topics[m][i].token === token) {
+                        topics[m].splice(i, 1);
+                        return token;
+                    }
+                }
+            }
+        }
+        return false;
+    };
+
+    getPubSubz = function () {
+        return pubsubz;
+    };
+
+    window.pubsubz = getPubSubz();
+
+}(this, this.document));
